fdf1110ef7bb72c0aefe96c75d8d9dcc779038aa
wheejuni <wheejuni@gmail.com>
2020-09-20 14:51:02 +0900

    뿌리기 이벤트 캐시 객체 테스트

--- showerhead-api/src/main/kotlin/com/wheejuni/showerhead/domain/CacheableSpreadEvent.kt
+++ showerhead-api/src/main/kotlin/com/wheejuni/showerhead/domain/CacheableSpreadEvent.kt
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 
 @RedisHash("spreadevent")
-class CacheableSpreadEvent(@Id private val eventId: String) {
+class CacheableSpreadEvent(@Id val eventId: String) {
 
     var receivedUserIds: List<String> = emptyList()
 }
--- showerhead-api/src/test/kotlin/com/wheejuni/showerhead/domain/CacheableSpreadEventTest.kt
+++ showerhead-api/src/test/kotlin/com/wheejuni/showerhead/domain/CacheableSpreadEventTest.kt
@@ -3,7 +3,7 @@ package com.wheejuni.showerhead.domain
 import com.wheejuni.showerhead.domain.repositories.cache.CacheableSpreadEventRepository
 import com.wheejuni.showerhead.domain.repositories.tid
 import com.wheejuni.showerhead.infra.cache.TestCacheConfig
-import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -20,16 +20,31 @@ internal class CacheableSpreadEventTest {
 
     @BeforeEach
     fun `캐시 초기화`() {
-        repository.deleteAll()
         repository.save(testableObject)
     }
 
     @Test
     fun `레포지토리 조회기능 테스트`() {
         //given
-        val maybeCacheEvent = repository.findByEventId(tid)
+        val maybeCacheEvent = repository.findById(tid)
 
         //then
-        assertDoesNotThrow { maybeCacheEvent!!.receivedUserIds }
+        assertTrue(maybeCacheEvent.isPresent)
+    }
+
+    @Test
+    fun `유저아이디 추가 기능 테스트`() {
+        //given
+        val event = repository.findById(tid)
+        val testUserIds = (0..3).map { it.toString() }
+
+        //when
+        val concreteEvent = event.get()
+        concreteEvent.receivedUserIds = testUserIds
+
+        repository.save(concreteEvent)
+
+        //then
+        assertEquals(testUserIds, repository.findById(tid).get().receivedUserIds)
     }
 }
