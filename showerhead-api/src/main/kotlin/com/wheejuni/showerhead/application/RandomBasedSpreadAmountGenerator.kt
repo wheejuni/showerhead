f9c6aa8c479e85aee6768da2406e85c4d0111cea
hwijun-jeong <hwijun.jeong@navercorp.com>
2020-09-21 13:36:09 +0900

    spread service 테스트 케이스 추가

--- showerhead-api/src/main/kotlin/com/wheejuni/showerhead/application/RandomBasedSpreadAmountGenerator.kt
+++ showerhead-api/src/main/kotlin/com/wheejuni/showerhead/application/RandomBasedSpreadAmountGenerator.kt
@@ -7,16 +7,17 @@ import java.util.*
 class RandomBasedSpreadAmountGenerator(val random: Random): SpreadAmountGenerator {
 
     override fun generateSpreadAmount(spreadEvent: SpreadEvent): List<SpreadAmount> {
+        var remainingAmount = spreadEvent.amount
+
         return (0 until spreadEvent.receiverCount)
                 .map mapper@{
-                    val remainingAmount = spreadEvent.amount
 
                     if (it == spreadEvent.receiverCount - 1) {
                         return@mapper SpreadAmount(remainingAmount)
                     }
 
                     val randomAmount = random.nextInt(remainingAmount)
-                    spreadEvent.amount -= randomAmount
+                    remainingAmount -= randomAmount
 
                     return@mapper SpreadAmount(randomAmount)
                 }
--- showerhead-api/src/test/kotlin/com/wheejuni/showerhead/application/SpreadServiceTest.kt
+++ showerhead-api/src/test/kotlin/com/wheejuni/showerhead/application/SpreadServiceTest.kt
@@ -6,14 +6,9 @@ import com.wheejuni.showerhead.view.handlerargument.RequesterIdentity
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.transaction.annotation.Transactional
-import java.util.stream.Stream
 
 const val TEST_ROOM_ID = "A1234"
 
@@ -40,7 +35,7 @@ internal class SpreadServiceTest {
 
     @Test
     @Transactional
-    fun `뿌리기 현황 조회 테스트`() {
+    fun `뿌리기 받기 테스트`() {
         //given
         val receiverId = "zella.ddo"
         val eventId = service.newEvent(testRequest, testIdentity)
@@ -52,6 +47,30 @@ internal class SpreadServiceTest {
         assertNotNull(amount)
         assertThrows(IllegalArgumentException::class.java) {
             service.getAmountOnRequest(eventId, RequesterIdentity(receiverId, TEST_ROOM_ID))
+        }
+    }
+
+    @Test
+    @Transactional
+    fun `뿌리기 현황조회 기능 테스트`() {
+        //given
+        val requesterId = "zella.ddo"
+        val testIdentity = RequesterIdentity(requesterId, TEST_ROOM_ID)
+
+        val eventId = service.newEvent(testRequest, testIdentity)
+
+        val invalidIdentity = RequesterIdentity(TESTABLE_SPREAD_EVENT_REQUESTER_ID, TEST_ROOM_ID)
+
+        //when
+        val fetchedEvent = service.getSpreadEventInfo(eventId, testIdentity)
+
+        //then
+        assertEquals(5, fetchedEvent.amounts.size)
+        assertEquals(TESTABLE_AMOUNT, fetchedEvent.amount)
+        assertTrue(fetchedEvent.valid)
+
+        assertThrows<SecurityException> {
+            service.getSpreadEventInfo(eventId, invalidIdentity)
         }
     }
 
