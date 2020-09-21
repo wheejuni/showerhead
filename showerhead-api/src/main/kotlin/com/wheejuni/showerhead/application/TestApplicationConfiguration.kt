b518d371df9f1cd10cb736e44cd2bd334216a4b1
hwijun-jeong <hwijun.jeong@navercorp.com>
2020-09-22 01:08:03 +0900

    amount generator 빈 등록 설정 변경

--- showerhead-api/src/main/kotlin/com/wheejuni/showerhead/application/TestApplicationConfiguration.kt
+++ showerhead-api/src/main/kotlin/com/wheejuni/showerhead/application/TestApplicationConfiguration.kt
@@ -3,12 +3,14 @@ package com.wheejuni.showerhead.application
 import com.wheejuni.showerhead.infra.cache.TestCacheConfig
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Import
+import org.springframework.context.annotation.Profile
 import java.util.*
 
 const val TEST_RECEIVER_COUNT = 5
 
 @Import(TestCacheConfig::class)
 @TestConfiguration
+@Profile("!local")
 class TestApplicationConfiguration: ApplicationConfiguration() {
 
     override fun randomAmountGenerator(): SpreadAmountGenerator {
